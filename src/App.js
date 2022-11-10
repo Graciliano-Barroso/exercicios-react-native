f74674cc148cb337d8f8f41173dd0765dbf9e652
Rico <grico484@gmail.com>
2022-11-10 17:09:41 -0400

    feat: Um pouco de estilo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 /* Primeiro componente */
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 
 import CompPadrao, { Comp1, Comp2 } from "./components/Mult";
 import Primeiro from './components/Primeiro';
 
 export default () => (
-    <SafeAreaView>
+    <SafeAreaView style={style.App}>
         <CompPadrao />
         <Comp1 />
         <Comp2 />
         <Primeiro />
     </SafeAreaView>
 );
+
+const style = StyleSheet.create({
+    App: {
+        backgroundColor: '#afafaf',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
