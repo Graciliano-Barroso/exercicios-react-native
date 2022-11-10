d6f86236bfad7c43f7f0adf5e6a7c5a44b363744
Rico <grico484@gmail.com>
2022-11-10 16:28:28 -0400

    feat: Entendendo melhor importação

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 
+import CompPadrao, { Comp1, Comp2 } from "./components/Mult";
 import Primeiro from './components/Primeiro';
 
 export default () => (
     <SafeAreaView>
+        <CompPadrao />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </SafeAreaView>
 );
