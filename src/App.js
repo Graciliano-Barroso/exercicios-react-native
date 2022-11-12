8fe580d053d5286e6233d34268d1fd6150a8c095
Rico <grico484@gmail.com>
2022-11-12 06:36:10 -0400

    feat: Número aleatório e destructuring

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 
-import MinMax from "./components/MinMax";
+import Aleatorio from "./components/Aleatorio";
+// import MinMax from "./components/MinMax";
 // import CompPadrao, { Comp1, Comp2 } from "./components/Mult";
 // import Primeiro from './components/Primeiro';
 
 export default () => (
     <SafeAreaView style={style.App}>
-        <MinMax min={3} max={20} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        <Aleatorio min={1} max={60} />
+        {/* <MinMax min={3} max={20} />
         <MinMax min='1' max='94' />
-        {/* <CompPadrao />
+        <CompPadrao />
         <Comp1 />
         <Comp2 />
         <Primeiro /> */}
