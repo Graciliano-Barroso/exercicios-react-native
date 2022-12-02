256724e69fd87ce6f2fa0ad63917a6f236d20e91
Graciliano-Barroso <grico484@gmail.com>
2022-12-01 22:15:14 -0400

    feat: Componente de classe: estado

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput } from 'react-native';
 import Estilo from '../estilo';
 
 export default class Mega extends Component {
 
+    state = {
+        qtdeNumeros: this.props.qtdeNumeros + 2000
+    }
+
+    alterarQtdeNumero(qtde) {
+        this.setState({ qtdeNumeros: qtde });
+    }
+
     render() {
         return (
-            <Text style={Estilo.txtG}>
-                Geerador de Mega-Sena {this.props.qtdeNumeros}
-            </Text>
+            <>
+                <Text style={Estilo.txtG}>
+                    Gerador de Mega-Sena {this.state.qtdeNumeros}
+                </Text>
+                <TextInput 
+                    placeholder='Quantidade de Números'
+                    value={this.state.qtdeNumeros}
+                    onChangeText={this.alterarQtdeNumero}
+                />
+            </>
         )
     }
 }
